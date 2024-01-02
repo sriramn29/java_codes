c369cb0a22f5927f4289362419e43aa08e5a9101
sriramn29 <139962755+sriramn29@users.noreply.github.com>
2024-01-02 13:51:27 +0530

    Add files via upload

---
+++ printing_names.java
@@ -0,0 +1,15 @@
+public class printing_names {
+    public static void main(String[] args){
+        int myNum = 5;
+        float myFloatNum = 5.99f;
+        char myLetter = 'D';
+        boolean myBool = true;
+        String myText = "Hello";
+        System.out.println(myNum);
+        System.out.println(myFloatNum);
+        System.out.println(myLetter);
+        System.out.println(myBool);
+        System.out.println(myText);
+    }
+// 
+}
---
+++ printing_numbers.java
@@ -0,0 +1,8 @@
+public class printing_numbers {
+    public static void main(String[] args){
+        //this is comment
+        System.out.println(2 - 2);
+        System.out.print(2 + 2);
+    }
+// 
+}
